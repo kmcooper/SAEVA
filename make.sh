927041754ab8af1eec395743cfe7167458c4393e
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2018-06-08 11:14:39 -0500

    Create make.sh

---
+++ make.sh
@@ -0,0 +1,19 @@
+pd= `pwd`
+cd $HOME
+mkdir data
+mkdir bin
+mkdir reference_genome
+cd reference_genome
+wget ftp://igenome:G3nom3s4u@ussd-ftp.illumina.com/Staphylococcus_aureus_NCTC_8325/NCBI/2006-02-13/Staphylococcus_aureus_NCTC_8325_NCBI_2006-02-13.tar.gz
+tar -xzf Staphylococcus_aureus_NCTC_8325_NCBI_2006-02-13.tar.gz
+rm Staphylococcus_aureus_NCTC_8325_NCBI_2006-02-13.tar.gz
+cd ..
+cd $pd
+mv *.sh $HOME/bin/
+cd $WORK
+## make trimmomatic directory
+mkdir trimmomatic
+cd trimmomatic
+mkdir trimlog
+## make BWA
+mkdir samfiles
