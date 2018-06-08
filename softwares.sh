5e33f5c9289a5f4d08cafba8ec5509ed7d485cba
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2018-06-08 12:44:53 -0500

    Update softwares.sh

--- softwares.sh
+++ softwares.sh
@@ -1,2 +1,9 @@
-wget https://sourceforge.net/projects/samtools/files/samtools/1.5/samtools-1.5.tar.bz2/download
+cd $WORK
+cd softwares
+wget https://sourceforge.net/projects/samtools/files/samtools/1.5/samtools-1.5.tar.bz2
 tar -xvjf samtools-1.5.tar.bz2
+rm samtools-1.5.tar.bz2
+cd samtools-1.5 # Within the unpacked release directory
+sh configure
+cd $pd
+    
