8b730e59e274bb6a3a197ca5e7a9689797f07acf
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2018-06-08 13:53:39 -0500

    Update makedir.sh

--- makedir.sh
+++ makedir.sh
@@ -1,12 +1,7 @@
 pd= `pwd`
 cd $HOME
 mkdir data
-mkdir bin
 mkdir reference_genome
-cd reference_genome
-wget ftp://igenome:G3nom3s4u@ussd-ftp.illumina.com/Staphylococcus_aureus_NCTC_8325/NCBI/2006-02-13/Staphylococcus_aureus_NCTC_8325_NCBI_2006-02-13.tar.gz
-tar -xzf Staphylococcus_aureus_NCTC_8325_NCBI_2006-02-13.tar.gz
-rm Staphylococcus_aureus_NCTC_8325_NCBI_2006-02-13.tar.gz
 cd $WORK
 ## make trimmomatic directory
 mkdir trimmomatic
