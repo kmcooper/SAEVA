dc2f3677858dd3d1fb3c78736eff56681768347d
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2018-06-14 18:48:45 -0500

    Update bwaScript.sh

--- bwaScript.sh
+++ bwaScript.sh
@@ -1,3 +1,8 @@
+cd $HOME/reference_genome
+wget ftp://igenome:G3nom3s4u@ussd-ftp.illumina.com/Staphylococcus_aureus_NCTC_8325/NCBI/2006-02-13/Staphylococcus_aureus_NCTC_8325_NCBI_2006-02-13.tar.gz
+tar -xzf Staphylococcus_aureus_NCTC_8325_NCBI_2006-02-13.tar.gz
+rm Staphylococcus_aureus_NCTC_8325_NCBI_2006-02-13.tar.gz
+
 cd $WORK/softwares/bwa
 wget https://sourceforge.net/projects/bio-bwa/files/bwa-0.7.17.tar.bz2
 tar -xvjf bwa-0.7.17.tar.bz2
