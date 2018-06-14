49c921acffc2b5ecf424f4b31bd061dbb6576317
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2018-06-14 18:11:02 -0500

    Update bamScript.sh

--- bamScript.sh
+++ bamScript.sh
@@ -2,6 +2,8 @@ cd $WORK/softwares/bwa
 wget https://sourceforge.net/projects/bio-bwa/files/bwa-0.7.17.tar.bz2
 tar -xvjf bwa-0.7.17.tar.bz2
 rm bwa-0.7.17.tar.bz2
+cd bwa-0.7.17
+make
 cd $WORK/SAEVA
 
 sbatch bam.slurm
