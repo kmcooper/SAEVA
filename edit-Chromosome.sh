836652abeeed22793f143f7f3d926905a0a8763b
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2018-06-18 15:06:01 -0500

    Create edit-Chromosome.sh

---
+++ edit-Chromosome.sh
@@ -0,0 +1,8 @@
+sed -i 's/^chr/Chromosome/' $WORK/high/0000.vcf;
+sed -i 's/^chr/Chromosome/' $WORK/high/0001.vcf;
+sed -i 's/^chr/Chromosome/' $WORK/high/0002.vcf;
+sed -i 's/^chr/Chromosome/' $WORK/high/0003.vcf;
+sed -i 's/^chr/Chromosome/' $WORK/high/0004.vcf;
+sed -i 's/^chr/Chromosome/' $WORK/low/0001.vcf;
+sed -i 's/^chr/Chromosome/' $WORK/low/0000.vcf; 
+
