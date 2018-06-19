85c9fb11f7aaf8408d05b52815580381ced76b08
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2018-06-19 17:11:26 -0500

    Update freebayesScript.sh

--- freebayesScript.sh
+++ freebayesScript.sh
@@ -1,6 +1,6 @@
-cd $WORK/softwares/freebayes/
+cd $WORK/SAEVA_softwares/freebayes/
 git clone --recursive git://github.com/ekg/freebayes.git
-cd freebayes
+cd SAEVA_softwares
 make
 cd $WORK/SAEVA
 sbatch freebayes.slurm
