094b0b00b088ca834a1d786cfcb512656df84ad2
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2018-06-19 18:04:15 -0500

    Update bwaScript.sh

--- bwaScript.sh
+++ bwaScript.sh
@@ -1,4 +1,5 @@
-cd $HOME/SAEVA_reference_genome
+cd $HOME
+cd SAEVA_reference_genome
 wget ftp://igenome:G3nom3s4u@ussd-ftp.illumina.com/Staphylococcus_aureus_NCTC_8325/NCBI/2006-02-13/Staphylococcus_aureus_NCTC_8325_NCBI_2006-02-13.tar.gz
 tar -xzf Staphylococcus_aureus_NCTC_8325_NCBI_2006-02-13.tar.gz
 rm Staphylococcus_aureus_NCTC_8325_NCBI_2006-02-13.tar.gz
