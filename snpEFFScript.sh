2ecab589b4d99aa37034754c560bd6c4dfe42e01
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2018-06-26 14:13:48 -0500

    Update snpEFFScript.sh

--- snpEFFScript.sh
+++ snpEFFScript.sh
@@ -1,7 +1,10 @@
 cd $WORK/SAEVA_softwares/snpEFF
 wget http://sourceforge.net/projects/snpeff/files/snpEff_latest_core.zip
+
 unzip snpEff_latest_core.zip
 rm snpEff_latest_core.zip
+
+
 cd $WORK/SAEVA
 sbatch snpEFF.slurm
 
