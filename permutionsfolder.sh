5b611d05ceb68a1ec7500491812c33c7b8c8e15e
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2018-07-07 16:11:44 -0500

    Create permutionsfolder.sh
    make folders to save the outputs of different persisters

---
+++ permutionsfolder.sh
@@ -0,0 +1,11 @@
+cd $WORK/SAVEA_outputs/
+mkdir changes
+cd changes
+mkdir 6highs4lows
+cd 6highs4lows
+mkdir 1
+cd 1
+mkdir snpEff-outputs
+cd snpEff-outputs
+mkdir snpEff-genes
+mkdir snpeff-summary
