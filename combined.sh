9adbd629eae8710f3871f166261a0b2c9c85e4f8
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2018-07-16 13:16:45 -0500

    Update combined.sh

--- combined.sh
+++ combined.sh
@@ -1,5 +1,6 @@
 cd $WORK/SAEVA_outputs/changes/7high3lows/1/snpEff_outputs/
-cat *.vcf | sort | grep -v '##'| grep -v '#' >$WORK/SAEVA_outputs/changes/7high3lows/1/combined.txt;
+cat *.vcf | sort | grep -v '##'| grep -v '#'| unic -c >$WORK/SAEVA_outputs/changes/7high3lows/1/combined.txt;
+
 
 
 cd $WORK/SAEVA_outputs/changes/7high3lows/2/snpEff_outputs/
