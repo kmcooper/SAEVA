8e45a75a0cfd311928b89552513783fd788ce704
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2018-07-16 09:52:34 -0500

    Update combined.sh

--- combined.sh
+++ combined.sh
@@ -1,3 +1,20 @@
 cd $WORK/SAEVA_outputs/changes/7high3lows/1/snpEff_outputs/
-
 cat *.vcf | sort | grep -v '##'>$WORK/SAEVA_outputs/changes/7high3lows/1/combined.txt;
+
+cd $WORK/SAEVA_outputs/changes/7high3lows/2/snpEff_outputs/
+cat *.vcf | sort | grep -v '##'>$WORK/SAEVA_outputs/changes/7high3lows/2/combined.txt;
+
+cd $WORK/SAEVA_outputs/changes/7high3lows/3/snpEff_outputs/
+cat *.vcf | sort | grep -v '##'>$WORK/SAEVA_outputs/changes/7high3lows/3/combined.txt;
+
+cd $WORK/SAEVA_outputs/changes/7high3lows/4/snpEff_outputs/
+cat *.vcf | sort | grep -v '##'>$WORK/SAEVA_outputs/changes/7high3lows/4/combined.txt;
+
+cd $WORK/SAEVA_outputs/changes/6high4lows/1/snpEff_outputs/
+cat *.vcf | sort | grep -v '##'>$WORK/SAEVA_outputs/changes/6high4lows/1/combined.txt;
+
+cd $WORK/SAEVA_outputs/changes/6high4lows/2/snpEff_outputs/
+cat *.vcf | sort | grep -v '##'>$WORK/SAEVA_outputs/changes/6high4lows/2/combined.txt;
+
+cd $WORK/SAEVA_outputs/changes/6high4lows/3/snpEff_outputs/
+cat *.vcf | sort | grep -v '##'>$WORK/SAEVA_outputs/changes/6high4lows/3/combined.txt;
