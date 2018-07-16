fae44a6cfaa96cf9bbe76d79279cd605ed824e54
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2018-07-15 21:19:59 -0500

    Update combined.sh

--- combined.sh
+++ combined.sh
@@ -1,2 +1,3 @@
 cd $WORK/SAEVA_outputs/changes/7high3lows/1/snpEff_outputs/
-cat *.vcf |sed -i 's/^Chromosome/' >$WORK/SAEVA_outputs/changes/7high3lows/1/combined.vcf;
+
+cat *.vcf | sort>$WORK/SAEVA_outputs/changes/7high3lows/1/combined.txt;
