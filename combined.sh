15f1a7cf938932c4d20f95a1dcb671a69e56c3ab
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2018-07-17 13:09:51 -0500

    Update combined.sh

--- combined.sh
+++ combined.sh
@@ -2,23 +2,23 @@ cd $WORK/SAEVA_outputs/changes/7high3lows/1/snpEff_outputs/
 echo `pwd`
 cat *.vcf | sort | grep -v '##'| grep -v '#' >$WORK/SAEVA_outputs/changes/7high3lows/1/combined.txt;
 cat $WORK/SAEVA_outputs/changes/7high3lows/1/combined.txt |cut -f -5|uniq -c |sort >$WORK/SAEVA_outputs/changes/7high3lows/1/countlines.txt
-result1=$(cat $WORK/SAEVA_outputs/changes/7high3lows/1/countlines.txt |grep -w "10"|cut -f2);
-cat /work/biocore/kimia/SAEVA_outputs/changes/7high3lows/1/combined.txt |grep -w $result1 |cut --complement -f6,9,10 | sed 's/AB=.*;ANN=/ANN=/' |uniq > $WORK/SAEVA_outputs/changes/7high3lows/1/results.txt
+result=$(cat $WORK/SAEVA_outputs/changes/7high3lows/1/countlines.txt |grep -w "10"|cut -f2);
+cat $WORK/SAEVA_outputs/changes/7high3lows/1/combined.txt |grep -w $result |cut --complement -f6,9,10 | sed 's/AB=.*;ANN=/ANN=/' |uniq > $WORK/SAEVA_outputs/changes/7high3lows/1/results.txt
 
 cd $WORK/SAEVA_outputs/changes/7high3lows/2/snpEff_outputs/
 echo `pwd`
 cat *.vcf | sort | grep -v '##'| grep -v '#'>$WORK/SAEVA_outputs/changes/7high3lows/2/combined.txt;
 cat $WORK/SAEVA_outputs/changes/7high3lows/2/combined.txt |cut -f -5|uniq -c |sort >$WORK/SAEVA_outputs/changes/7high3lows/2/countlines.txt
-result2=$(cat $WORK/SAEVA_outputs/changes/7high3lows/2/countlines.txt |grep -w "10"|cut -f2);
-cat $WORK/SAEVA_outputs/changes/7high3lows/2/combined.txt |grep -w $result2| uniq -c > $WORK/SAEVA_outputs/changes/7high3lows/2/results.txt
+result=$(cat $WORK/SAEVA_outputs/changes/7high3lows/2/countlines.txt |grep -w "10"|cut -f2);
+cat $WORK/SAEVA_outputs/changes/7high3lows/2/combined.txt |grep -w $result |cut --complement -f6,9,10 | sed 's/AB=.*;ANN=/ANN=/' |uniq > $WORK/SAEVA_outputs/changes/7high3lows/2/results.txt
 
 
 cd $WORK/SAEVA_outputs/changes/7high3lows/3/snpEff_outputs/
 echo `pwd`
 cat *.vcf | sort | grep -v '##'| grep -v '#'>$WORK/SAEVA_outputs/changes/7high3lows/3/combined.txt;
 cat $WORK/SAEVA_outputs/changes/7high3lows/1/combined.txt |cut -f -5|uniq -c |sort >$WORK/SAEVA_outputs/changes/7high3lows/3/countlines.txt
-result3=$(cat $WORK/SAEVA_outputs/changes/7high3lows/3/countlines.txt |grep -w "10"|cut -f2);
-cat $WORK/SAEVA_outputs/changes/7high3lows/3/combined.txt |grep -w $result3| uniq -c > $WORK/SAEVA_outputs/changes/7high3lows/3/results.txt
+result=$(cat $WORK/SAEVA_outputs/changes/7high3lows/3/countlines.txt |grep -w "10"|cut -f2);
+cat $WORK/SAEVA_outputs/changes/7high3lows/3/combined.txt |grep -w $result |cut --complement -f6,9,10 | sed 's/AB=.*;ANN=/ANN=/' |uniq > $WORK/SAEVA_outputs/changes/7high3lows/3/results.txt
 
 
 cd $WORK/SAEVA_outputs/changes/7high3lows/4/snpEff_outputs/
@@ -26,26 +26,28 @@ echo `pwd`
 cat *.vcf | sort | grep -v '##'| grep -v '#'>$WORK/SAEVA_outputs/changes/7high3lows/4/combined.txt;
 cat $WORK/SAEVA_outputs/changes/7high3lows/1/combined.txt |cut -f -5|uniq -c |sort >$WORK/SAEVA_outputs/changes/7high3lows/4/countlines.txt
 result4=$(cat $WORK/SAEVA_outputs/changes/7high3lows/4/countlines.txt |grep -w "10"|cut -f2);
-cat $WORK/SAEVA_outputs/changes/7high3lows/4/combined.txt |grep -w $result4 | uniq -c > $WORK/SAEVA_outputs/changes/7high3lows/4/results.txt
+cat $WORK/SAEVA_outputs/changes/7high3lows/4/combined.txt |grep -w $result |cut --complement -f6,9,10 | sed 's/AB=.*;ANN=/ANN=/' |uniq > $WORK/SAEVA_outputs/changes/7high3lows/4/results.txt
 
 
 cd $WORK/SAEVA_outputs/changes/6highs4lows/1/snpEff_outputs/
 echo `pwd`
 cat *.vcf | sort | grep -v '##'| grep -v '#'>$WORK/SAEVA_outputs/changes/6highs4lows/1/combined.txt;
 cat $WORK/SAEVA_outputs/changes/6highs4lows/1/combined.txt |cut -f -5|uniq -c |sort >$WORK/SAEVA_outputs/changes/6highs4lows/1/countlines.txt
-result5=$(cat $WORK/SAEVA_outputs/changes/6highs4lows/1/countlines.txt |grep -w "10"|cut -f2);
-cat $WORK/SAEVA_outputs/changes/6highs4lows/1/combined.txt | grep -w $result5| uniq -c > $WORK/SAEVA_outputs/changes/6highs4lows/1/results.txt
+result=$(cat $WORK/SAEVA_outputs/changes/6highs4lows/1/countlines.txt |grep -w "10"|cut -f2);
+cat $WORK/SAEVA_outputs/changes/6highs4lows/1/combined.txt |grep -w $result |cut --complement -f6,9,10 | sed 's/AB=.*;ANN=/ANN=/' |uniq > $WORK/SAEVA_outputs/changes/6highs4lows/1/results.txt
 
 
 cd $WORK/SAEVA_outputs/changes/6highs4lows/2/snpEff_outputs/
+echo `pwd`
 cat *.vcf | sort | grep -v '##'| grep -v '#'>$WORK/SAEVA_outputs/changes/6highs4lows/2/combined.txt;
 cat $WORK/SAEVA_outputs/changes/6highs4lows/2/combined.txt |cut -f -5|uniq -c |sort >$WORK/SAEVA_outputs/changes/6highs4lows/2/countlines.txt
-result6=$(cat $WORK/SAEVA_outputs/changes/6highs4lows/2/countlines.txt |grep -w "10"|cut -f2);
-cat $WORK/SAEVA_outputs/changes/6highs4lows/2/combined.txt |grep -w $result6 | uniq -c > $WORK/SAEVA_outputs/changes/6highs4lows/2/results.txt
+result=$(cat $WORK/SAEVA_outputs/changes/6highs4lows/2/countlines.txt |grep -w "10"|cut -f2);
+cat $WORK/SAEVA_outputs/changes/6highs4lows/2/combined.txt |grep -w $result |cut --complement -f6,9,10 | sed 's/AB=.*;ANN=/ANN=/' |uniq > $WORK/SAEVA_outputs/changes/6highs4lows/2/results.txt
 
 
 cd $WORK/SAEVA_outputs/changes/6highs4lows/3/snpEff_outputs/
+echo `pwd`
 cat *.vcf | sort | grep -v '##'| grep -v '#'>$WORK/SAEVA_outputs/changes/6highs4lows/3/combined.txt;
 cat $WORK/SAEVA_outputs/changes/6highs4lows/3/combined.txt |cut -f -5|uniq -c |sort >$WORK/SAEVA_outputs/changes/6highs4lows/3/countlines.txt
-result7=$(cat $WORK/SAEVA_outputs/changes/6highs4lows/3/countlines.txt |grep -w "10"|cut -f2);
-cat $WORK/SAEVA_outputs/changes/6highs4lows/3/combined.txt |grep -w $result7| uniq -c > $WORK/SAEVA_outputs/changes/6highs4lows/3/results.txt
+result=$(cat $WORK/SAEVA_outputs/changes/6highs4lows/3/countlines.txt |grep -w "10"|cut -f2);
+cat $WORK/SAEVA_outputs/changes/6highs4lows/3/combined.txt |grep -w $result |cut --complement -f6,9,10 | sed 's/AB=.*;ANN=/ANN=/' |uniq > $WORK/SAEVA_outputs/changes/6highs4lows/3/results.txt
