f2beaca4d56ecb0973f1f656b56def64ebf6daf4
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2018-07-25 22:10:09 -0500

    Create filters.sh

---
+++ filters.sh
@@ -0,0 +1,15 @@
+sbatch 4low-filters.slurm;
+sbatch 7high-filters.slurm;
+sbatch 7high-4low-filters.slurm;
+####################################
+sbatch 6high-4low-filter1.slurm;
+sbatch 6high-4low-filter2.slurm;
+sbatch 6high-4low-filter3.slurm;
+sbatch 6high-4low-filter4.slurm;
+sbatch 6high-4low-filter5.slurm;
+sbatch 6high-4low-filter6.slurm;
+###################################
+sbatch 7high-3low-filter1.slurm;
+sbatch 7high-3low-filter2.slurm;
+sbatch 7high-3low-filter3.slurm;
+sbatch 7high-3low-filter4.slurm;
