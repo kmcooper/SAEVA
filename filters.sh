ade75359eedd654544a98aa58cbe5661686aa6a2
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2018-07-30 15:07:12 -0500

    Update filters.sh

--- filters.sh
+++ filters.sh
@@ -8,6 +8,7 @@ sbatch 6high-4low-filter3.slurm;
 sbatch 6high-4low-filter4.slurm;
 sbatch 6high-4low-filter5.slurm;
 sbatch 6high-4low-filter6.slurm;
+sbatch 6high-4low-filter7.slurm;
 ###################################
 sbatch 7high-3low-filter1.slurm;
 sbatch 7high-3low-filter2.slurm;
