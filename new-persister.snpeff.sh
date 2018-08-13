7ba71b1c7cb5ad6abee0f4acfbbb0e1ffdc347fc
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2018-08-12 19:58:02 -0500

    Update new-persister.snpeff.sh

--- new-persister.snpeff.sh
+++ new-persister.snpeff.sh
@@ -1,3 +1,4 @@
+sbatch new-7high-4low-snpEff.slurm;
 sbatch new-6high4low-snpEff1.slurm;
 sbatch new-6high4low-snpEff2.slurm;
 sbatch new-6high4low-snpEff3.slurm;
@@ -5,7 +6,7 @@ sbatch new-6high4low-snpEff4.slurm;
 sbatch new-6high4low-snpEff5.slurm;
 sbatch new-6high4low-snpEff6.slurm;
 sbatch new-6high4low-snpEff7.slurm;
-sbatch new-7high43low-snpEff1.slurm;
-sbatch new-7high43low-snpEff2.slurm;
-sbatch new-7high43low-snpEff3.slurm;
-sbatch new-7high43low-snpEff4.slurm;
+sbatch new-7high3low-snpEff1.slurm;
+sbatch new-7high3low-snpEff2.slurm;
+sbatch new-7high3low-snpEff3.slurm;
+sbatch new-7high3low-snpEff4.slurm;
