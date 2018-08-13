e737e05053a04c81816e432892ae2fee0661e75f
Kimia Ameri <32547598+kimiaameri@users.noreply.github.com>
2018-08-12 19:44:48 -0500

    Create new-persister.snpeff.sh

---
+++ new-persister.snpeff.sh
@@ -0,0 +1,11 @@
+sbatch new-6high4low-snpEff1.slurm;
+sbatch new-6high4low-snpEff2.slurm;
+sbatch new-6high4low-snpEff3.slurm;
+sbatch new-6high4low-snpEff4.slurm;
+sbatch new-6high4low-snpEff5.slurm;
+sbatch new-6high4low-snpEff6.slurm;
+sbatch new-6high4low-snpEff7.slurm;
+sbatch new-7high43low-snpEff1.slurm;
+sbatch new-7high43low-snpEff2.slurm;
+sbatch new-7high43low-snpEff3.slurm;
+sbatch new-7high43low-snpEff4.slurm;
